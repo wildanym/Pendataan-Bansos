d406aa81f21ac2c76d906bd2ff0ca1c0334253fa
wildanym <wildanym@gmail.com>
2022-04-11 08:07:52 +0700

    upadate success page

--- src/components/SuccessMsg.vue
+++ src/components/SuccessMsg.vue
@@ -10,7 +10,9 @@
 			</div>
 			<img src="../assets/success.png" alt="success" class="w-24" />
 			<h1 class="text-xl text-gray-800">Berhasil dikirim</h1>
-			<button class="btn btn-send mb-3" @click="$emit('home')">Selesai</button>
+			<button class="btn h-12 pb-4 btn-send mb-3" @click="$emit('home')">
+				Selesai
+			</button>
 		</div>
 	</div>
 </template>
